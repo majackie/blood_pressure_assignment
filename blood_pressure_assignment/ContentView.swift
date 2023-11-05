53d62de564ddfabe4a756d7c6c0f31fec277020f
Jackie Ma <97272592+majackie@users.noreply.github.com>
2023-11-05 10:59:31 -0800

    Initial Commit

---
+++ blood_pressure_assignment/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  blood_pressure_assignment
+//
+//  Created by Jackie Ma on 2023-11-05.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ blood_pressure_assignment/blood_pressure_assignmentApp.swift
@@ -0,0 +1,17 @@
+//
+//  blood_pressure_assignmentApp.swift
+//  blood_pressure_assignment
+//
+//  Created by Jackie Ma on 2023-11-05.
+//
+
+import SwiftUI
+
+@main
+struct blood_pressure_assignmentApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
